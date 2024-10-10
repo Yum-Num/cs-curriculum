be2818f26d6570cc019cbb5be2a3f5d548918824
Yum-Num <ynakamura9447@student.pps.net>
2024-10-10 14:03:35 -0700

    movement direction
    femboy hooters

--- Assets/Scripts/Enemy AI.cs
+++ Assets/Scripts/Enemy AI.cs
@@ -10,14 +10,17 @@ public class EnemyAI : MonoBehaviour
     public List<GameObject> points;
     public int pointer = 0;
     public GameObject player;
-    public States state;
+    public States state = States.Wander;
+    public Animator animation;
     public enum States
     {
         Wander,
-        Chase
+        Chase,
+        Attack
     }
     void Update()
     {
+        Vector3 movementVector = default;
         if (state == States.Wander)
         {
             if (Vector3.Distance(transform.position, points[pointer].transform.position) < 0.01f)
@@ -25,8 +28,40 @@ public class EnemyAI : MonoBehaviour
                 pointer++;
                 pointer %= points.Count;
             }
-            transform.position = Vector3.MoveTowards(transform.position, points[pointer].transform.position, 1 * Time.deltaTime);    
+            
+            movementVector = Vector3.MoveTowards(transform.position, points[pointer].transform.position, 1 * Time.deltaTime);
+            transform.position = movementVector;
         }
+
+        if (state == States.Chase)
+        {
+            movementVector = Vector3.MoveTowards(transform.position, player.transform.position, 1f * Time.deltaTime);
+            transform.position = movementVector;
+            if (Vector3.Distance(transform.position, player.transform.position) < 1f)
+            {
+                state = States.Attack;
+            }
+        }
+
+        if (state == States.Attack)
+        {
+            Debug.Log("attacking");
+        }
+
+        movementVector = movementVector.normalized;
+        if (Mathf.Abs(movementVector.x) > Mathf.Abs(movementVector.y))
+        {
+            if (Mathf.Sign())
+            {
+                animation.Play();
+            }
+            
+        } 
+        else if (Mathf.Abs(movementVector.y) > Mathf.Abs(movementVector.x))
+        {
+            //do y stuff
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -34,6 +69,12 @@ public class EnemyAI : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             player = other.gameObject;
+            state = States.Chase;
         }
+    }
+
+    private void onTriggerExit2d(Collider2D other)
+    {
+        state = States.Wander;
     }
 }
